274278430196bfbc2c7ff79ceef8143d8b3c9638
PhoenixSmith <phoenix.smith.bcbsnc@gmail.com>
2018-08-09 16:55:45 -0400

    Updated JS to include color error and no text repeat

--- script.js
+++ script.js
@@ -3,37 +3,54 @@
 document.getElementById('submit-button').addEventListener('click', function(){
     event.preventDefault()
 
+    clearError1()
 
     var name = document.getElementById('name').value.trim()
 
     if (name !== ""){
-        document.getElementById('name').innerText = 'Proceed to next step'
-        validName()
-    } else {
+        document.getElementById('name')
+        validName() 
+    } else{
         emptyError1()
     }
+        
 })
 
 function validName(){
     var validDiv = document.createElement('div')
     validDiv.classList.add('input-valid')
-    validDiv.innerText = 'Proceed to next step'
+
 
     var field = document.getElementById('name-field')
     field.appendChild(validDiv)
-    field.classList.add('valid')
+    field.classList.add('input-valid')
 }
 
-function emptyError1(errorDiv){
+function emptyError1(){
     var errorDiv = document.createElement('div')
-    errorDiv.classList.add('input-invalid')
+    errorDiv.classList.add('error-message')
     errorDiv.innerText = 'This field is required'
 
     var field = document.getElementById('name-field')
     field.appendChild(errorDiv)
-    field.classList.add('error')
+    field.classList.add('input-invalid')
 
 }
+
+function clearError1() {
+    var fields = document.getElementsByClassName('input-field')
+    for (var field of fields){
+        field.classList.remove('error-message')
+    }
+
+
+    var errorMessage = document.getElementsByClassName('error-message')[0]
+    // var errorMessage = field.querySelectorAll('.input-field')
+    console.log(errorMessage)
+    if (errorMessage) {
+      errorMessage.remove()
+    }
+  }
 
 //Car - Year, Make, Model Note: 3 event listeners
 
